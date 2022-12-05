5980a9adf37260fa807c5d9403717b74bf762abf
[redacted] <r3tr0_02@protonmail.com>
2022-12-06 01:36:25 +0800

    Alpha test 1.3
    + Added new rule `laptop(MacBook)`
    * `reformatting and comments`

--- Assignment_5Baka.pl
+++ Assignment_5Baka.pl
@@ -1,3 +1,4 @@
+%--------------------------Main run-------------------
 run:-
 	write('<<WELCOME TO THE LAPTOP PICKER>>'),
 	nl,
@@ -5,8 +6,9 @@ run:-
 	nl,
 	laptop(Laptop).
 
-
+% if not found laptop
 run:-
+	nl,
 	write('Sorry, I dont seem to be able to find a suitable laptop'),nl.
 
 %--------------------------LAPTOP---------------------
@@ -33,44 +35,76 @@ laptop(Jade) :-
 	spec(power_consuming),
 	spec(has_dedicated_gpu),
 	write('The laptop i will recommend you: Jade').
-laptop(Lolipop):-
-	spec(asthetic),
+
+laptop(Lolipop) :-
+	spec(aesthetic),
 	spec(work),
 	spec(battery_life),
-	write('The laptop i will recommend you: Lolipop' ).
+	write('The laptop i will recommend you: Lolipop').
+
+laptop(MacBook) :-
+	spec(aesthetic),
+	spec(work),
+	spec(battery_life),
+	spec(expensive),
+	write('The laptop i will recommend you: MacBook').
 
 %-------------------------ASK----------------------
+% To ask whether or not user is a gamer
 spec(gamer):-
-	verify("Are you a gamer? (y/n)").
-spec(work):-
-	verify("Are you using your laptop for your work? (y/n)").
-spec(battery_life):-
-	verify("Are you always outside? (y/n)").
-spec(gaming):-
-	verify("Are you looking for a gaming laptop? (y/n)").
-spec(heavy):-
-	verify("Do you want a heavy laptop? (y/n)").
-spec(expensive):-
-	verify("Do u have a high budget? (y/n)").
-spec(power_consuming):-
-	verify("Do u want a high power laptop? (y/n)").
-spec(has_dedicated_gpu):-
-	verify("Do you want your laptop have a dedicated GPU? (y/n)").
-spec(asthetic):-
-	verify("Do you want your laptop to be asthetic? (y/n)").
-	
+	nl,
+	verify("Are you a gamer? (y/n) : ").
 
+% To ask whether or not user uses laptop for work
+spec(work):-
+	nl,
+	verify("Are you using your laptop for your work? (y/n) : ").
+
+% To ask whether or not user uses laptop on battery
+spec(battery_life):-
+	nl,
+	verify("Are you always outside? (y/n) : ").
+
+% To ask whether or not user are looking for gaming laptop
+spec(gaming):-
+	nl,
+	verify("Are you looking for a gaming laptop? (y/n) : ").
+
+% To ask whether or not user wants a heavy or lightweight
+spec(heavy):-
+	nl,
+	verify("Do you want a heavy laptop? (y/n) : ").
+
+% To ask whether or not user is poor / rich lol
+spec(expensive):-
+	nl,
+	verify("Do u have a high budget? (y/n) : ").
+
+% To ask whether or not user is a huge power consumer
+spec(power_consuming):-
+	nl,
+	verify("Do u want a high power laptop? (y/n) : ").
+
+% To ask whether or not user wants GPU
+spec(has_dedicated_gpu):-
+	nl,
+	verify("Do you want your laptop have a dedicated GPU? (y/n) : ").
+
+% To ask whether or not user wants MacBook LOL
+spec(aesthetic):-
+	nl,
+	verify("Do you want your laptop to be aesthetic? (y/n) : ").
+	
 
 %------------------------FUNCTION-------------------
 verify(Question) :-
-   (yes(Question) -> true ;
-    (no(Question) -> fail ;
-     ask(Question))).
+   	(yes(Question) -> true ;
+	(no(Question) -> fail ;
+     	ask(Question))).
 
-ask(Question):-
+ask(Question) :-
 	write(Question),
 	read(N),
-	( (N == yes ; N == y)
-      ->
-       assert(yes(Question)) ;
-       assert(no(Question)), fail).
+	( (N == yes ; N == y) ->
+       	assert(yes(Question)) ;
+       	assert(no(Question)), fail).
