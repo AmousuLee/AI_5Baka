d7ab89bdcc986f649ab423b6e77381749ef278ac
AmousuLee <leeamous@gmail.com>
2022-12-06 11:17:51 +0800

    Alt Version
    Might replace the main branch one

--- Assignment_5Baka.pl
+++ Assignment_5Baka.pl
@@ -11,169 +11,138 @@ run:-
 	nl,
 	write('Sorry, I dont seem to be able to find a suitable laptop'),nl.
 
-%--------------------------LAPTOP---------------------
-laptop(AzuzZeppy) :-
+%--------------------------LAPTOP-----------------------------------
+laptop(azuz_zephy) :- azuz_zephy, !.
+laptop(jade) :- jade,!.
+laptop(msi) :- msi,!.
+laptop(ivm) :- ivm,!.
+laptop(joi) :- joi,!.
+laptop(unknown).
+
+%--------------------------LAPTOP IDENTIFICATION---------------------
+azuz_zephy :-
 	spec(gaming),
+	spec(small_battery),
+	spec(expensive),
 	spec(heavy),
-	spec(expensive),
-	spec(power_consuming),
-	spec(has_dedicated_gpu),
-	spec(port),
-	write('The laptop i will recommend you: AzuzZeppy').
+	spec(screen),
+	write('The laptop i will recommend you: Azuz Zephy').
 
-
-laptop(AzuzFake) :-
+jade :-
 	spec(gaming),
+	spec(big_battery),
+	spec(budget_cheap),
+	spec(light),
 	spec(webcam),
-	spec(expensive),
-	spec(power_consuming),
-	spec(has_dedicated_gpu),
-	write('The laptop i will recommend you: AzuzFake').
-
-laptop(Squidware) :-
-	spec(gaming),
-	spec(gamer),
-	spec(battery_life),
-	spec(budget_range),
-	spec(has_dedicated_gpu),
-	write('The laptop i will recommend you: Squidware').
-
-
-laptop(Jade) :-
-	spec(gamer),
-	spec(work),
-	spec(battery_life),
-	spec(power_consuming),
-	spec(has_dedicated_gpu),
 	spec(screen),
 	write('The laptop i will recommend you: Jade').
 
-
-laptop(Lolibook) :-
-	spec(work),
-	spec(screen),
-	spec(battery_life),
-	spec(webcam),
-	spec(port),
-	write('The laptop i will recommend you: Lolibook').
-
-laptop(msi) :-
-	spec(aesthetic),
-	spec(gamer),
-	spec(battery_life),
+msi :-
+	spec(gaming),
+	spec(small_battery),
+	spec(expensive),
+	spec(heavy),
 	spec(webcam),
 	write('The laptop i will recommend you: MSI').
 
-laptop(JOI) :-
-	spec(battery_life),
-	spec(work),
-	spec(budget_range),
-	write('The laptop i will recommend you: JOI').
-	
 
-laptop(MacBook) :-
-	spec(aesthetic),
-	spec(work),
+ivm :-
+	spec(work_study),
+	spec(big_battery),
+	spec(budget),
+	spec(light),
 	spec(webcam),
-	spec(expensive),
-	write('The laptop i will recommend you: MacBook').
-
-laptop(aSUSyBaka) :-
-	spec(study),
-	spec(battery_life).
-	write('The laptop i will recommend you: aSuSyBaka').
-
-laptop(IVM) :-
-	spec(battery_life),
-	spec(port),
-	spec(work),
-	spec(budget_range).
 	write('The laptop i will recommend you: IVM').
 
+joi :-
+	spec(work_study),
+	spec(big_battery),
+	spec(expensive),
+	spec(light),
+	spec(webcam),
+	spec(screen),
+	write('The laptop i will recommend you: JOI').
 
-%-------------------------ASK----------------------
-% To ask whether or not user is a gamer
-spec(gamer):-
-	nl,
-	verify("Are you a gamer? (y/n) : " ).
 
+%-------------------------ASK ----------------------
 % To ask whether or not user uses laptop for work
-spec(work):-
+spec(work_study):-
 	nl,
-	verify("Are you using your laptop for your work? (y/n) : ").
-
-% To ask whether or not user uses laptop on battery
-spec(battery_life):-
-	nl,
-	verify("Are you always outside? (y/n) : ").
+	verify("Are you using your laptop for your work and/or study?").
 
 % To ask whether or not user are looking for gaming laptop
 spec(gaming):-
 	nl,
-	verify("Are you looking for a gaming laptop? (y/n) : ").
+	verify("Are you looking for a gaming laptop?").
 
-% To ask whether or not user wants a heavy or lightweight
+% To ask whether or not user want a laptop with big battery
+spec(big_battery):-
+	nl,
+	verify("Are you always outside and need big battery?").
+
+% To ask whether or not user want a laptop with small battery
+spec(small_battery):-
+	nl,
+	verify("Do u mind a laptop with smaller battery?").
+
+% To ask whether or not user wants a heavy laptop
 spec(heavy):-
 	nl,
-	verify("Do you want a heavy laptop? (y/n) : ").
+	verify("Do you mind a heavy laptop?").
 
-% To ask whether or not user is poor / rich lol
+% To ask whether or not user wants a lightweight laptop
+spec(light):-
+	nl,
+	verify("Do you want a light laptop?").
+
+
+% To ask whether or not user is rich 
 spec(expensive):-
 	nl,
-	verify("Do u have a high budget? (y/n) : ").
+	verify("Do u have a high budget?").
+
 
 % To ask whether the user budget is withn range
-spec(budget_range):-
+spec(budget_cheap):-
 	nl,
-	verify("Is your laptop budget below RM2000? (y/n) : ").
-
-
-% To ask whether or not user is a huge power consumer
-spec(power_consuming):-
-	nl,
-	verify("Do u want a high power laptop? (y/n) : ").
+	verify("Is your laptop budget below RM3000?").
 
 % To ask whether or not user wants GPU
 spec(has_dedicated_gpu):-
 	nl,
-	verify("Do you want your laptop have a dedicated GPU? (y/n) : ").
+	verify("Do you want your laptop have a dedicated GPU?").
 
-% To ask whether or not user wants MacBook LOL
-spec(aesthetic):-
-	nl,
-	verify("Do you want your laptop to be aesthetic? (y/n) : ").
-
-% To ask whether or not user wants a webcam LOL
+% To ask whether or not user wants a webcam
 spec(webcam):-
 	nl,
-	verify("Do you want your laptop to have a webcam? (y/n) : ").
+	verify("Do you want your laptop to have a webcam?").
 
-% To ask whether the user wants a big screen
+
+% To ask whether the user wants a color accurate screen
 spec(screen):-
 	nl,
-	verify("Do you want your laptop to have a 15inch screen? (y/n) : ").
-
-% To ask whether or not user uses laptop for study
-spec(study):-
-	nl,
-	verify("Are you using your laptop for study? (y/n) : ").
-	
-% To ask user if their required a lot of port for work
-spec(port):-
-	n1,
-	verify("Are you going to need use a lot of port? (y/n) : ").
-
+	verify("Do you want your laptop to have a color accurate screen?").
 	
 
 %------------------------FUNCTION-------------------
+ask(Question) :-
+	write(Question),
+	write('(y/n)'), nl,
+	write('>:'),
+	read(Answer),
+	nl,
+	( (Answer == yes ; Answer == y)
+	 ->
+	assert(yes(Question)) ;
+	assert(no(Question)), fail).
+
+:- dynamic yes/1,no/1.
+
 verify(Question) :-
    	(yes(Question) -> true ;
 	(no(Question) -> fail ;
-     	ask(Question))).
+	ask(Question))).
 
-ask(Question) :-
-	write(Question),
-	read(N),
-	( (N == yes ; N == y) ->
-       	assert(yes(Question)) ;
-       	assert(no(Question)), fail).
+undo :- retract(yes(_)),fail.
+undo :- retract(no(_)),fail.
+undo.
